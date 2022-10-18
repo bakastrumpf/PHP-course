b3599e546d91f4ea742614114ca67051101bfba0
bakastrumpf <baka.strumpf@gmail.com>
2022-10-18 20:17:41 +0200

    ticks and nrs | blow ur mind

--- 16js/primer163mojBroj.js
+++ 16js/primer163mojBroj.js
@@ -2,18 +2,18 @@
 
 let startTime = undefined;
 
-function zeroPad(number, length){
+function zeroPad(number, length) {
     let result = number.toString();
     let n = length - result.length;
 
     let i = 0;
-    for(i=0; i<n; ++i){
+    for (i = 0; i < n; ++i) {
         result = "0" + result;
     }
     return result;
 }
 
-function tick(){
+function tick() {
     let currentTime = new Date();
     let difference = currentTime.getTime() - startTime.getTime();
 
@@ -32,9 +32,66 @@ function tick(){
     document.getElementById("timer").innerHTML = timer;
 }
 
-function start(){
-    // pišemo funkciju jer želimo da se pokreće klikom na dugme, a ne čim se stranica učita
-    startTime = new Date();
+let set0 = [10, 15, 20];
+let set1 = [25, 50, 75, 100];
+
+
+let goal = 0;
+let digit1 = 0;
+let digit2 = 0;
+let digit3 = 0;
+let digit4 = 0;
+let index0 = 0;
+let index1 = 0;
+
+function generate() {
+    goal = Math.random()*1000;
+    goal = Math.floor(goal);
+
+    digit1 = Math.random()*(10-1)+1;
+    digit2 = Math.random()*(10-1)+1;
+    digit3 = Math.random()*(10-1)+1;
+    digit4 = Math.random()*(10-1)+1;
     
-    setInterval(tick, 100);
+    digit1 = Math.floor(digit1);
+    digit2 = Math.floor(digit2);
+    digit3 = Math.floor(digit3);
+    digit4 = Math.floor(digit4);
+
+    index0 = Math.random()*set0.length;
+    index0 = Math.floor(index0);
+
+    index1 = Math.random()*set1.length;
+    index1 = Math.floor(index1);
+
+    document.getElementById("goal").innerHTML = zeroPad(goal, 3);
+    document.getElementById("digit1").innerHTML = digit1;
+    document.getElementById("digit2").innerHTML = digit2;
+    document.getElementById("digit3").innerHTML = digit3;
+    document.getElementById("digit4").innerHTML = digit4;
+    document.getElementById("number0").innerHTML = set0[index0];
+    document.getElementById("number1").innerHTML = set1[index1];
+}
+
+let state = undefined;
+// 0 = generate
+// 1 = play
+
+let generateId = undefined;
+let tickId = undefined;
+
+function start() {
+    // pišemo funkciju jer želimo da se pokreće klikom na dugme, a ne čim se stranica učita
+    if (state == undefined || state == 1) {
+        document.getElementById("dugmence").innerHTML = "Stop";
+        clearInterval (tickId);
+        generateId = setInterval(generate, 100);
+        state = 0;
+    } else {
+        document.getElementById("dugmence").innerHTML = "Počni";
+        clearInterval(generateId);
+        startTime = new Date();
+        tickId = setInterval(tick, 100);
+        state = 1;
+    }
 }
