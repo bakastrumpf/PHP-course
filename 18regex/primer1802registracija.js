b93f0729506f118c77a066aa927896087e6a07f9
bakastrumpf <baka.strumpf@gmail.com>
2022-10-22 12:19:46 +0200

    phone function

--- 18regex/primer1802registracija.js
+++ 18regex/primer1802registracija.js
@@ -6,13 +6,36 @@ function proveriKorisnickoIme() {
     return match != null;
 }
 
+function proveriMejl(){
+    var mejl = document.getElementById("mejl").value;
+    // alert(mejl);
+    var reg = new RegExp("^([a-z0-9_\\.-]+)@([a-z]+\\.)+[a-z]{2,10}$","i");
+    var match = mejl.match(reg);
+    // alert(match); => za tasha@etf.rs => ["tasha@etf.rs", "tasha", "etf."]
+    return match!=null;
+}
+
+function proveriTelefon(){
+    var tel = document.getElementById("telefon").value;
+    var reg = new RegExp("^\\d+$");
+    var pos = tel.search(reg);
+    // var match = tel.match(reg);
+    return pos != -1;
+}
+
 function proveriPodatke() {
     var poruka = "";
     if (!proveriKorisnickoIme())
-        poruka += "Korisničko ime nije ispravno";
+        poruka += "Korisničko ime nije ispravno. ";
     
+    if(!proveriMejl())
+        poruka += "Mejl nije u ispravnom formatu. ";
+
+    if(!proveriTelefon())
+        poruka += "Telefon nije u ispravnom formatu. ";
+
     if (poruka == "")
-        poruka = "Sve je u redu";
+        poruka = "Sve je u redu. Uspešno ste se registrovali.";
     document.getElementById("poruka").innerHTML = poruka;
         
 }
