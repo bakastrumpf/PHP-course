6739753ec0dd58530dbfdd2342318ee0c289c590
bakastrumpf <baka.strumpf@gmail.com>
2022-11-13 23:42:35 +0100

    phone added

--- O-DOMACI/D19bio/d0559script.js
+++ O-DOMACI/D19bio/d0559script.js
@@ -7,6 +7,14 @@ function proveriMejl() {
     return match != null;
 }
 
+function proveriTel() {
+    var tel = document.getElementById("tel");
+    var reg = new RegExp("^\\+?(\\(\\d+\\)|\\d+/?)[\\d ]+$");
+    var pos = tel.search(reg);
+    // var match = tel.match(reg);
+    return pos != -1;
+}
+
 function proveriPoruku() {
     var poruka = document.getElementById("poruka").value;
     if (poruka == null)
@@ -17,6 +25,8 @@ function posalji() {
     var poruka = "";
     if (!proveriMejl())
         poruka += "Нисте унели имејл адресу. ";
+    if (!proveriTel())
+        poruka += "Телефон није у исправном формату. ";
     if (!proveriPoruku())
         poruka += "Нисте унели поруку. ";
     if (poruka == "")
