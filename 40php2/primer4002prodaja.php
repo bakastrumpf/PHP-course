36bd480aaf73fbd235c9fd2ae72d2300cd47ee9e
bakastrumpf <baka.strumpf@gmail.com>
2022-12-17 14:57:16 +0100

    meta data added

--- 40php2/primer4002prodaja.php
+++ 40php2/primer4002prodaja.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+    <meta name="keywords" content="Prekvalifikacije, ETF, PHP, html5" />
+    <meta name="author" content="KŽ | MS" />
+    <meta name="description" content="PHP: PHP basics" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>#PHP40: prodaja</title>
